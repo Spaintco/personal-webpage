d1b0846b1fd3279021f3ba1c618f4b75671bd7db
Sebastian Pinto <pinto7lopez7@gmail.com>
2021-07-08 22:58:30 -0500

    Envio de mensajes funcionando

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
 
@@ -21,6 +22,28 @@ def projects():
 @app.route('/studies')
 def studies():
     return render_template('studies.html')
+
+@app.route('/message', methods=['POST'])
+def message():
+    if request.method == 'POST':
+
+        ### lectura de datos del formulario ###
+        nombre = str(request.form['nombre'])
+        email = str(request.form['email'])
+        mensaje = str(request.form['mensaje'])
+
+        gmailaddress = "web.mail.sebpin@gmail.com"
+        gmailpassword = "WebSite1!"
+        mailto = "pinto7lopez7@gmail.com"
+        msg = "My name is {}, my email is {} and my message is {}".format(nombre, email, mensaje)
+        mailServer = smtplib.SMTP('smtp.gmail.com' , 587)
+        mailServer.starttls()
+        mailServer.login(gmailaddress , gmailpassword)
+        mailServer.sendmail(gmailaddress, mailto , msg)
+        mailServer.quit()
+
+        return render_template('message.html')
+        
 
 
 if __name__ == '__main__':
